ee8a25bf24f72975303e74cbe191ace4463a3b8f
Gero Herkenrath <gero@gero-herkenrath.de>
2017-05-12 21:37:44 +0200

    added needed classes and set up storyboard

--- SOExDynamicButtonGen/DataCreatorController.swift
+++ SOExDynamicButtonGen/DataCreatorController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DataCreatorController.swift
 //  SOExDynamicButtonGen
 //
 //  Created by Ghost in the Shell on 12.05.17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DataCreatorController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func doDynamic(_ sender: Any) {
+	}
 
 }
 
---
+++ SOExDynamicButtonGen/LeafController.swift
@@ -0,0 +1,37 @@
+//
+//  LeafController.swift
+//  SOExDynamicButtonGen
+//
+//  Created by Ghost in the Shell on 12.05.17.
+//  Copyright © 2017 Gero Herkenrath. All rights reserved.
+//
+
+import UIKit
+
+class LeafController: UIViewController {
+
+	@IBOutlet weak var leafNameLabel: UILabel!
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
